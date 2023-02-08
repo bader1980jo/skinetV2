d28ddd54dce91300f26a5cf7252a76a035a4b17d
bader1980jo <bader.salah@gmail.com>
2023-02-08 20:57:44 +0400

    test rules1

--- Core/Specifications/BaseSpicification.cs
+++ Core/Specifications/BaseSpicification.cs
@@ -13,7 +13,7 @@ namespace Core.Specifications
             Criteria = criteria;
             Includes = includes;
         }
-
+//test
         public Expression<Func<T, bool>> Criteria {get;}
 
         public List<Expression<Func<T, object>>> Includes {get;} = 
